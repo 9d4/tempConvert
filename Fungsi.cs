2c9e1e724ac7bc53a76c29ec169d4905b1584dd0
Dimas Ananda <traperwaze@gmail.com>
2019-11-30 13:36:44 +0700

    complete Fungsi.cs

--- Fungsi.cs
+++ Fungsi.cs
@@ -7,10 +7,73 @@ namespace tempConvert
 {
     class Fungsi
     {
+        //=====
+        //to Fahrenheit
+        //from C
         public decimal CToF(decimal value)
         {
             decimal result = (value * 1.8M) + 32;
             return result;
+        }
+
+        //from R
+        public decimal RToF(decimal value)
+        {
+            return (value * 2.25M) + 32;
+        }
+
+        //from K
+        public decimal KToF(decimal value)
+        {
+            return (value * 1.8M) - 459.67M;
+        }
+
+        //===========
+        //to Celcius
+        //from Fahrn
+        public decimal FToC(decimal value)
+        {
+            return (value - 32M) * (5M / 9M);
+        }
+
+        //from Reamur
+        public decimal RToC(decimal value)
+        {
+            return value * (5M / 4M);
+        }
+
+        //from kelvin
+        public decimal KToC(decimal value)
+        {
+            return value - 273.15M;
+        }
+
+        //==========   
+        //to Reamur
+        //from celcius
+        public decimal CToR(decimal value)
+        {
+            return value * (4M / 5M);
+        }
+
+        //from Fahrn
+        public decimal FToR(decimal value)
+        {
+            return (value - 32) * (4M / 9M);
+        }
+
+        //from Kelvin
+        public decimal KToR(decimal value)
+        {
+            return (value - 273.15M) * (4M / 5M);
+        }
+
+        //=========
+        //to Kelvin
+        //from Celcius
+        public decimal CToK(decimal value)
+        {
+            return value + 273.15M;
         }
     }
 }
